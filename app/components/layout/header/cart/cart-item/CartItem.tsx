a882dc28ac8eb0d869ff599b50f4bf9993e3181a
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2024-07-22 13:59:33 +0300

    feat: implemented markup for CartItem

--- app/components/layout/header/cart/cart-item/CartItem.tsx
+++ app/components/layout/header/cart/cart-item/CartItem.tsx
@@ -1,10 +1,27 @@
 import Image from 'next/image'
 import { FC } from 'react'
 
-export const CartItem: FC = () => {
+import { ICartItem } from '@/types/cart-item.interface'
+import styles from './CartItem.module.css'
+
+export const CartItem: FC<{ item: ICartItem }> = ({ item }) => {
 	return (
-		<div>
-			<Image src={} alt='Image Cart' />
+		<div className={styles.item}>
+			<Image
+				src={item.product.images[0]}
+				alt={item.product.name}
+				width={100}
+				height={100}
+			/>
+			<div>
+				<div className={styles.names}>{item.product.name}</div>
+				<div className={styles.price}>
+					{new Intl.NumberFormat(undefined, {
+						style: 'currency',
+						currency: 'USD',
+					}).format(item.product.price)}
+				</div>
+			</div>
 		</div>
 	)
 }
