9d277debbdc61bc69d0ac10fe061b6d679080ae3
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2024-07-22 13:39:11 +0300

    feat: implemented interfaces for the shopping cart

--- app/components/layout/header/cart/Cart.tsx
+++ app/components/layout/header/cart/Cart.tsx
@@ -1,5 +1,16 @@
-const Cart = () => {
-	return <div>Cart</div>
+import { FC } from 'react'
+import styles from './Cart.module.css'
+
+const Cart: FC = () => {
+	return (
+		<div className={styles['wrapper-cart']}>
+			<div className={styles.heading}>
+				<span className={styles.badge}>1</span>
+				<span className={styles.text}>My basket</span>
+			</div>
+			<div className={styles.cart}></div>
+		</div>
+	)
 }
 
 export default Cart
---
+++ app/components/layout/header/cart/cart-item/CartItem.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import { FC } from 'react'
+
+export const CartItem: FC = () => {
+	return (
+		<div>
+			<Image src={} alt='Image Cart' />
+		</div>
+	)
+}
