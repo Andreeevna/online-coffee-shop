a99f42d7bf93a937acd647de905e0656d100fcf0
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2024-07-31 14:51:00 +0300

    feat: added store and types

---
+++ app/store/store.ts
@@ -0,0 +1,11 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+
+const rootReducer = combineReducers({
+	cart: cartSlice.reducer,
+})
+
+export const store = configureStore({
+	reducer: rootReducer,
+})
+
+export type TypeRootState = ReturnType<typeof rootReducer>
