67930916ec8f7939ac22e732d1679119b6aa7806
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2024-08-06 11:10:19 +0300

    feat: we took out the receipt of cart and total in the hook

--- app/store/store.ts
+++ app/store/store.ts
@@ -1,7 +1,16 @@
 'use client'
 
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
-import { persistReducer, persistStore } from 'redux-persist'
+import {
+	FLUSH,
+	PAUSE,
+	PERSIST,
+	persistReducer,
+	persistStore,
+	PURGE,
+	REGISTER,
+	REHYDRATE,
+} from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 import { cartSlice } from './slice'
 
@@ -19,6 +28,12 @@ const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
 	reducer: persistedReducer,
+	middleware: getDefaultMiddleware =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+			},
+		}),
 })
 
 export const persistor = persistStore(store)
