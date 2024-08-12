fcc04ddc83f0426a92109a2ab15e36e650ed97b2
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2024-08-12 14:54:49 +0300

    feat: added colors for export

---
+++ app/config/color.config.ts
@@ -0,0 +1,12 @@
+export const colors = {
+	green: '#008D64',
+	'dark-green': '#006044',
+	'light-green': '#E6F2EF',
+	black: '#222222',
+	white: '#ffffff',
+	red: '#F23C3D',
+	gray: '#E8E7E3',
+	brown: '#B6B4B3',
+	brige: '#A49B8F',
+	'transparent-color': '#1C00ff00',
+}
