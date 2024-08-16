68d0f7f365b904ec9ffb94cecc3cd9d1f0f81624
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2024-08-16 17:49:58 +0300

    feat: implemented deletion from the trash in the element

--- app/components/ui/catalog/carousel/carousel-item/CarouselButton.tsx
+++ app/components/ui/catalog/carousel/carousel-item/CarouselButton.tsx
@@ -1,7 +1,9 @@
 'use client'
 
 import { useActions } from '@/hooks/useActions'
+import { useCart } from '@/hooks/useCart'
 import { TypeSize } from '@/store/cart/types'
+import { ICartItem } from '@/types/cart-item.interface'
 import { IProduct } from '@/types/product.interface'
 import { Dispatch, FC, SetStateAction } from 'react'
 
@@ -16,20 +18,29 @@ const CarouselButton: FC<ICarouselButton> = ({
 	selectedSize,
 	setSelectedSize,
 }) => {
-	const { addToCart } = useActions()
+	const { addToCart, removeFromCart } = useActions()
+	const { cart } = useCart()
+	console.log(selectedSize)
+
+	const currentEl = cart.find(
+		(item: ICartItem) =>
+			item.product.id === product.id && item.size === selectedSize
+	)
 
 	return (
 		<button
 			onClick={() =>
-				addToCart({
-					product,
-					quantity: 1,
-					size: selectedSize,
-				})
+				currentEl
+					? removeFromCart({ id: currentEl.id })
+					: addToCart({
+							product,
+							quantity: 1,
+							size: selectedSize,
+					  })
 			}
 			className='tracking-widest bg-white rounded-xl px-3 py-1 text-xs mt-6 font-normal uppercase text-dark-green'
 		>
-			Add to backet
+			{currentEl ? 'Remove from basket' : 'Add to basket'}
 		</button>
 	)
 }
