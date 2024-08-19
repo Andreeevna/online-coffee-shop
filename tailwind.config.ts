38d57dc0fd1d3b54b1c45748f24de22b368576f9
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2024-08-19 11:09:20 +0300

    feat: implemented button animation with zoom

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -37,9 +37,18 @@ const config: Config = {
 						opacity: '1',
 					},
 				},
+				scaleIn: {
+					'0%': { opacity: '0', transform: 'scale(0.9)' },
+					'50%': { opacity: '0.3' },
+					'100%': {
+						opacity: '1',
+						transform: 'scale(1)',
+					},
+				},
 			},
 			animation: {
 				opacity: 'animationOpacity 0.5s ease-in-out',
+				scaleIn: 'scaleIn .35s ease-in-out',
 			},
 		},
 	},
